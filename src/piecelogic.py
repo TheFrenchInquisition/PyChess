7967dacede1a4d98ae137b9bc40b7f7edc025c73
Zerf <zerfgog202@gmail.com>
2014-12-23 16:45:28 -0700

    Update piecelogic.py
    Add bishop
    Update queen

--- src/piecelogic.py
+++ src/piecelogic.py
@@ -6,11 +6,13 @@ def rookLogic(piece, newpos):
 	return ((piece.pos[0] == newpos[0]) or (piece.pos[1] == newpos[1]))
 piecelogic["_Rook"]=rookLogic
 
+def bishopLogic(piece, newpos):
+	return (abs(piece.pos[1]-newpos[1]) == abs(piece.pos[0]-newpos[0]))
+piecelogic["_Bishop"]=bishopLogic
+
 def queenLogic(piece, newpos):
-	if rookLogic(piece, newpos):
-		return rookLogic(piece, newpos)
-	else:
-		return abs(piece.pos[1]-newpos[1]) == abs(piece.pos[0]-newpos[0])
+	return (rookLogic(piece, newpos) or bishopLogic(piece, newpos))
+
 piecelogic["_Queen"]=queenLogic
 
 #Pull from table of logics
