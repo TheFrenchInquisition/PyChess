5f0659a8c4a6d91d27f621e7dc4eaecb2553bb8a
Yvonne Cummings <aidantwothousand@gmail.com>
2014-12-23 16:02:22 -0700

    Queen
    Aside from being able to pass through pieces diagonally, the queen
    works.

--- src/piecelogic.py
+++ src/piecelogic.py
@@ -1,4 +1,16 @@
+import math
+
 def pieceCanMove(piece, newpos):
 	if piece.name.endswith("_Rook"):
 		return ((piece.pos[0] == newpos[0]) or (piece.pos[1] == newpos[1]))
+	if piece.name.endswith("_Queen"):
+		if piece.pos[0] == newpos[0]:
+			return (abs(piece.pos[1]-newpos[1]) == 1)
+		elif piece.pos[1] == newpos[1]:
+			return (abs(piece.pos[0]-newpos[0]) == 1)
+		else:
+			if abs(piece.pos[1]-newpos[1]) == abs(piece.pos[0]-newpos[0]):
+				return True
+			else:
+				return False
 	return True
