78914bd342f46e6ee800e9a01d8954f94c4ad061
Zerf <zerfgog202@gmail.com>
2014-12-23 15:22:14 -0700

    Update piecelogic.py
    Rook

--- src/piecelogic.py
+++ src/piecelogic.py
@@ -1,2 +1,6 @@
-def pieceCanMove(piece):
+def pieceCanMove(piece, newpos):
+	if piece.name.endswith("_Rook"):
+		print(piece.pos[0], piece.pos[1], newpos[0], newpos[1])
+		print ((piece.pos[0] == newpos[0]) or (piece.pos[1] == newpos[1]))
+		pass
 	return True
