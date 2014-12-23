291051e00750a229c120992b27d2d84bf297f08d
Zerf <zerfgog202@gmail.com>
2014-12-22 21:56:07 -0700

    Update main.py
    Made everything great

--- src/main.py
+++ src/main.py
@@ -1,169 +1,131 @@
-import pygame, sys
+import pygame, sys, os, math
 pygame.init()
 
-size = [400, 400]
+tilew, tileh = 45, 45
+height = tileh * 8
+width = tilew * 8
+
+size = [width, height]
+
+os.environ["SDL_VIDEO_CENTERED"] = "1"
+
 screen = pygame.display.set_mode(size)
- 
+
 pygame.display.set_caption("Chess")
 
 #load images and stuff
-wpawnimg = pygame.image.load("..\images\wpawn.png").convert_alpha()
-wrookimg = pygame.image.load("..\images\wrook.png").convert_alpha()
-wknightimg = pygame.image.load("..\images\wknight.png").convert_alpha()
-wbishopimg = pygame.image.load("..\images\wbishop.png").convert_alpha()
-wqueenimg = pygame.image.load("..\images\wqueen.png").convert_alpha()
-wkingimg = pygame.image.load("..\images\wking.png").convert_alpha()
-selectimg = pygame.image.load("..\images\select.png").convert_alpha()
+wpawnimg = pygame.image.load("../images/wpawn.png").convert_alpha()
+wrookimg = pygame.image.load("../images/wrook.png").convert_alpha()
+wknightimg = pygame.image.load("../images/wknight.png").convert_alpha()
+wbishopimg = pygame.image.load("../images/wbishop.png").convert_alpha()
+wqueenimg = pygame.image.load("../images/wqueen.png").convert_alpha()
+wkingimg = pygame.image.load("../images/wking.png").convert_alpha()
+selectimg = pygame.image.load("../images/select.png").convert_alpha()
 
-somethingIsSelected = False
-selected = (0, 0)
-move = (0, 0)
-flow = True
-mpos = (0, 0)
-alliedPiece = False
-player1 = 'w'
+selected = False
+
+EMPTY = 0 #Even though 0 is shorter to type..
+WPAWN = 1
+WROOK = 2
+WKNIG = 3
+WBISH = 4
+WQUEE = 5
+WKING = 6
+
+pieceimgs = {
+	WPAWN: wpawnimg,
+	WROOK: wrookimg,
+	WKNIG: wknightimg,
+	WBISH: wbishopimg,
+	WQUEE: wqueenimg,
+	WKING: wkingimg,
+}
 
 board = [
-    ['#', '#', '#', '#', '#', '#', '#', '#'],
-    ['#', '#', '#', '#', '#', '#', '#', '#'],
-    ['#', '#', '#', '#', '#', '#', '#', '#'],
-    ['#', '#', '#', '#', '#', '#', '#', '#'],
-    ['#', '#', '#', '#', '#', '#', '#', '#'],
-    ['#', '#', '#', '#', '#', '#', '#', '#'],
-    ['#', '#', '#', '#', '#', '#', '#', '#'],
-    ['#', '#', '#', '#', '#', '#', '#', '#'],
-    ]
-#function getCPos takes a raw xy position on the board ex:(218, 129) and returns a chess position ex:(5, 6)
-def getCPos(rpos):
-    rxpos = rpos[0]-20
-    if rxpos <= 45:
-        nxpos = 1
-    elif rxpos <=90:
-        nxpos = 2
-    elif rxpos <=135:
-        nxpos = 3
-    elif rxpos <=180:
-        nxpos = 4
-    elif rxpos <=225:
-        nxpos = 5
-    elif rxpos <=270:
-        nxpos = 6
-    elif rxpos <=315:
-        nxpos = 7
-    elif rxpos <=360:
-        nxpos = 8
-    else:
-        return False
-    rypos = rpos[1]-20
-    if rypos <= 45:
-        nypos = 8
-    elif rypos <=90:
-        nypos = 7
-    elif rypos <=135:
-        nypos = 6
-    elif rypos <=180:
-        nypos = 5
-    elif rypos <=225:
-        nypos = 4
-    elif rypos <=270:
-        nypos = 3
-    elif rypos <=315:
-        nypos = 2
-    elif rypos <=360:
-        nypos = 1
-    else:
-        return False
-    return (nxpos, nypos)
-#this does the opposite. will return value at top left corner of cpos
-def getRPos(cpos):
-    nxpos = cpos[0]*45-25
-    nypos = cpos[1]*-45+380
-    return (nxpos, nypos)
-#function drawPieces takes characters from board[] and draws pieces to the screen depending upon their value
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+#Makes sure x is between min and max
+def math_clamp(x, min, max): return x < min and min or (x > max and max or x)
+
+def isFree(pos):
+	row, col = pos[0], pos[1]
+	if ((row < 0) or (col < 0) or (row > len(board)) or (col > len(board))): return
+
+	return (board[row][col] == EMPTY)
+
+#Returns top left pixel value of grid pos
+def pixelpos(pos):
+	return (pos[0] * tilew, pos[1] * tileh)
+
+#Draws board pieces depending on value
 def drawPieces():
-    global selected
-    global somethingIsSelected
-    global flow
-    global mpos
-    global selected
-    for i in range(8):
-        for k in range(8):
-            ok = 7-k
-            if board[i][k] == 'WP':
-                screen.blit(wpawnimg, (i*45+20, ok*45+20))
-            elif board[i][k] == 'WR':
-                screen.blit(wrookimg, (i*45+20, ok*45+20))
-            elif board[i][k] == 'WN':
-                screen.blit(wknightimg, (i*45+20, ok*45+20))
-            elif board[i][k] == 'WB':
-                screen.blit(wbishopimg, (i*45+20, ok*45+20))
-            elif board[i][k] == 'WQ':
-                screen.blit(wqueenimg, (i*45+20, ok*45+20))
-            elif board[i][k] == 'WK':
-                screen.blit(wkingimg, (i*45+20, ok*45+20))
-            if somethingIsSelected:
-                if flow:   
-                    flow = False
-                screen.blit(selectimg, getRPos(selected))
-            else:
-                flow = True
-#function cPiece takes a piece type and desired position ex:('WK', 5, 1) and sets position in board[]
-def cPiece(piece, posl, posn):
-    board[posl-1][posn-1] = piece
-def checkIfAllied(cpos):
-    checkpiece = board[cpos[0]-1][cpos[1]-1]
-    whitepieces = ['WP', 'WR', 'WN', 'WB', 'WQ', 'WK']
-    blackpieces = ['BP', 'BR', 'BN', 'BB', 'BQ', 'BK']
-    if player1 == 'w':
-        if checkpiece in whitepieces:
-            return True
-        else:
-            return False
-    else:
-        if checkpiece == blackpieces:
-            return True
-        else:
-            return False
+	global selected
+	for i in range(len(board)):
+		row = board[i]
+		for i2 in range(len(row)):
+			square = row[i2]
+			if square != EMPTY:
+				screen.blit(pieceimgs[square], (i*45, i2*45))
+
+			if selected != False:
+				screen.blit(selectimg, pixelpos(selected))
+
+def cPiece(piece, pos):
+	board[pos[0]][pos[1]] = piece
+
 def move(opos, npos):
-    board[npos[0]-1][npos[1]-1] = board[opos[0]-1][opos[1]-1]
-    board[opos[0]-1][opos[1]-1] = '#'
+	global selected
+	board[npos[0]][npos[1]] = board[opos[0]][opos[1]]
+	board[opos[0]][opos[1]] = 0
+	selected = False
+
 def select(pos):
-    global somethingIsSelected
-    global selected
-    global move
-    if somethingIsSelected and not checkIfAllied(pos):
-        move(selected, pos)
-        somethingIsSelected = False
-    elif not somethingIsSelected and checkIfAllied(pos):
-        selected = pos
-        somethingIsSelected = True
+	global selected
+	selected = pos
+
 screen.fill((102, 102, 102))
 
-cPiece('WP', 1, 2)
-cPiece('WR', 1, 1)
-cPiece('WN', 2, 1)
-cPiece('WB', 3, 1)
-cPiece('WQ', 4, 1)
-cPiece('WK', 5, 1)
+cPiece(WPAWN, (0, 1))
+cPiece(WROOK, (0, 0))
+cPiece(WKNIG, (1, 0))
+cPiece(WBISH, (2, 0))
+cPiece(WQUEE, (3, 0))
+cPiece(WKING, (4, 0))
 
 clock = pygame.time.Clock()
-#loopedy loop
-while True:
-    
-    clock.tick(30)
-    
-    pygame.draw.rect(screen, (139, 125, 107), [20, 20, 360, 360])
-    for i in range(4):
-        for k in range(4):
-            pygame.draw.rect(screen, (255, 228, 196), [i*90+20, k*90+20, 45, 45])
-            pygame.draw.rect(screen, (255, 228, 196), [i*90+65, k*90+65, 45, 45])
 
-    drawPieces()
-    for event in pygame.event.get(): 
-        if event.type == pygame.QUIT: 
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.MOUSEBUTTONUP and getCPos(pygame.mouse.get_pos()) is not False:
-            
-            select(getCPos(pygame.mouse.get_pos()))
-    pygame.display.flip()
+while True:
+	clock.tick(10)
+
+	pygame.draw.rect(screen, (139, 125, 107), [0, 0, width, height])
+	for row in range(4):
+		for col in range(4):
+			pygame.draw.rect(screen, (255, 228, 196), [row*90, col*90, tilew, tileh])
+			pygame.draw.rect(screen, (255, 228, 196), [row*90+tilew, col*90+tileh, tilew, tileh])
+
+	drawPieces()
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+		elif event.type == pygame.MOUSEBUTTONUP:
+			(mouseX, mouseY) = pygame.mouse.get_pos()
+			curCol = int(math.ceil(mouseY/tileh))
+			curRow = int(math.ceil(mouseX/tilew))
+
+			if selected == False:
+				if not isFree((curRow, curCol)):
+					select((curRow, curCol))
+			else:
+				move(selected, (curRow, curCol))
+
+	pygame.display.flip()
