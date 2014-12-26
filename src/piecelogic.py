cd38e05873fe91c616f8c8dfe8c9b3f9b03298e8
Zerf <zerfgog202@gmail.com>
2014-12-26 14:57:23 -0700

    Update piecelogic.py
    Clean up Knight logic
    (named the variable)

--- src/piecelogic.py
+++ src/piecelogic.py
@@ -11,7 +11,9 @@ def rookLogic(piece, newpos):
 piecelogic["_Rook"]=rookLogic
 
 def knightLogic(piece, newpos):
-	return not (abs(piece.pos[0]-newpos[0]) > 2 or abs(piece.pos[1]-newpos[1]) > 2 or abs(piece.pos[0]-newpos[0]) == 0 or abs(piece.pos[1]-newpos[1]) == 0 or abs(piece.pos[0]-newpos[0]) == abs(piece.pos[1]-newpos[1]))
+	diffx = abs(piece.pos[0]-newpos[0])
+	diffy = abs(piece.pos[1]-newpos[1])
+	return (diffx <= 2 and diffy <= 2 and diffx != 0 and diffy != 0 and diffx != diffy)
 piecelogic["_Knight"]=knightLogic
 
 def bishopLogic(piece, newpos):
@@ -28,7 +30,7 @@ piecelogic["_King"]=kingLogic
 
 def pawnLogic(piece, newpos):
 	squaresallowed = piece.hasMoved and 1 or 2 #piece.hasmoved and 1 or 2
-	movedh = piece.name.startswith("W_") and (piece.pos[0] - newpos[0]) or (newpos[0] - piece.pos[0])
+	movedh = piece.name.startswith("W_") and (piece.pos[0] - newpos[0]) or (newpos[0] - piece.pos[0]) #Can't use abs like we would otherwise because pawns can't move backwards
 	movedv = piece.name.startswith("W_") and (piece.pos[1] - newpos[1]) or (newpos[1] - piece.pos[1])
 	if tempboard[newpos[0]][newpos[1]] != 0 and (bishopLogic(piece, newpos) and (abs(movedh) == 1 and movedv == 1)):
 		return True
